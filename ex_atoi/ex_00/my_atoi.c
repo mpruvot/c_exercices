b1725f64b0b51ba474f6623e126303991d55101a
mpruvot <132161864+mpruvot@users.noreply.github.com>
2023-08-15 14:44:30 +0200

    my atoi upddate from scratch + tests

--- ex_atoi/ex_00/my_atoi.c
+++ ex_atoi/ex_00/my_atoi.c
@@ -2,46 +2,113 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int is_nbr(char c)
+{
+    if(c >= '0' && c <= '9')
+        return 1;
+    else
+        return 0;    
+}
+
+int is_space(char c)
+{
+    if(c == ' ')
+        return 1;
+    else
+        return 0;      
+
+}
+
+int is_letter(char c)
+{
+    if (c <= 'z' && c >= 'a')
+        return (1);
+    if (c <= 'Z' && c >= 'A')
+        return (1);
+    else    
+        return 0;
+}
+
 int     my_atoi(const char *nptr) // 
 {
-    int number;
-    int sign;
     int i;
+    int sign;
+    int result;
 
-    number = 0;
-    sign = 0;
     i = 0;
-    while (nptr[i] >= 9 && nptr[i] <= 13 || nptr[i] == 32)
-    {
-        i++;
-    }
-    while (nptr[i] == '-' || nptr[i] == '+')
+    sign = 1;
+    result = 0;
+
+    if (!nptr)
+        return(0);
+    while (!is_nbr(nptr[i]))
     {
         if (nptr[i] == '-')
-            sign++;
+            sign = -1;
+        if (is_letter(nptr[i]))
+            return (0);
         i++;
     }
-    while (nptr[i] >= '0' && nptr[i] <= '9')
+    while (nptr[i] && is_nbr(nptr[i]) && !is_space(nptr[i]))
     {
-        number *= 10;
-        number += nptr[i] - '0';
+        result *= 10;
+        result += (nptr[i] - 48);
         i++;
     }
-    if (!(sign % 2))
-        return (number);
+    if (sign == -1)
+        return(result * sign);
     else
-        return (-number);
+        return(result);   
 }
 
-int main()
+
+int main() 
 {
-    int i;
-    char *s;
-    s = "968856566666666666666";
-    i = my_atoi(s);
-    printf("i = %d\n", i);
-    return (0);
+    struct Test {
+        char *input;
+        int expected;
+        char *name;
+    };
+
+    struct Test tests[] = {
+        {"123", 123, "Nombre positif"},
+        {"-123", -123, "Nombre négatif"},
+        {"0", 0, "Zéro"},
+        {"   123", 123, "Espaces au début"},
+        {"123   ", 123, "Espaces à la fin"},
+        {"   123   ", 123, "Espaces au début et à la fin"},
+        {"+123", 123, "Plus devant le nombre"},
+        {"-0", 0, "Zéro négatif"},
+        {"0000123", 123, "Zéros au début"},
+        {"-0000123", -123, "Zéros au début avec négatif"},
+        {"", 0, "Chaîne vide"},
+        {"a123", 0, "Lettre avant le nombre"},
+        {"123a", 123, "Lettre après le nombre"},
+        {"2147483647", 2147483647, "Limite INT_MAX"},
+        {"-2147483648", -2147483648, "Limite INT_MIN"},
+        // Ajoutez plus de tests si nécessaire.
+    };
+
+    int num_tests = sizeof(tests) / sizeof(tests[0]);
+
+    for (int i = 0; i < num_tests; i++) 
+    {
+        int result = my_atoi(tests[i].input);
+        if (result == tests[i].expected) 
+        {
+            printf("Test %d: \"%s\" -- Ok (Expected: %d, Got: %d)\n", i+1, tests[i].name, tests[i].expected, result);
+        } 
+        else 
+        {
+            printf("Test %d: \"%s\" -- Failure (Expected: %d, Got: %d)\n", i+1, tests[i].name, tests[i].expected, result);
+        }
+    }
+     
 }
+
+
+
+
 
 
 
